a6c3cc2a73e42cc6d49d5d81f164908bda440ce0
DommSite <johan.johansson06@live.se>
2024-02-07 10:21:17 +0100

    Add project files.

---
+++ inlämningsuppgift4lists/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace checkava
+{
+    class Program
+    {
+        static bool FinnsTecknetIAllaSträngar(string[] ord, char tecken)
+        {
+            for (int i = 0; i < ord.Length; i++)
+            {
+                if (ord[i].ToLower().Contains(tecken.ToString().ToLower()))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        static void Main()
+        {
+            string[] namn = { "Johan", "Programmering", "Självklart" };
+            Console.WriteLine(FinnsTecknetIAllaSträngar(namn, 'a'));
+        }
+    }
+}
